8a51aa834ca12086b73be5644b7f70af78902a3a
Jaylanzb <ljzb123@gmail.com>
2018-02-02 18:36:23 +0800

    h

--- test/src/com/lan/test/Test1.java
+++ test/src/com/lan/test/Test1.java
@@ -2,6 +2,18 @@ package com.lan.test;
 
 public class Test1 {
 	public static void main(String[] args) {
+//		JSONArray jsonArray = new JSONArray();
+//		JSONObject jsonObject = jsonArray.getJSONObject(0);
+//		System.out.println(jsonObject);
+//		JSONObject itemObject = new JSONObject();
+//		itemObject.put("createTime", "sdfsdf");
+//		JSONObject jsonObject = itemObject.getJSONObject("xxxxxxxxx");
+//		System.out.println(jsonObject);
+		
+//		ArrayList<Object> arrayList = new ArrayList<>();
+//		for (Object object : arrayList) {
+//			System.out.println(object);
+//		}
 		
 	}
 }
